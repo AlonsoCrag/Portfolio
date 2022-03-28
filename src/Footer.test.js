c20872fbca5c33bce196b72cc96222ed5167d15f
Alonso <alonsocadev@gmail.com>
2022-03-28 08:45:35 -0600

    Projects done

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Footer from './components/footer';
+import React from "react";
+import renderer from "react-test-renderer";
+import Footer from "./components/footer";
 
-test('Footer has been created', () => {
-    let tree = renderer.create(<Footer pos="absolute" />)
-    .toJSON();
-    expect(tree).toMatchInlineSnapshot(`
-<footer
-  className="bottom-0 border-gray-200 w-full px-2 sm:px-4 py-2.5 bg-[#131313] absolute"
->
-  <div
-    className=" container flex flex-wrap justify-center items-center container mx-auto max-w-7xl"
-  >
-    <p
-      className="text-white font-bold"
+test("Footer has been created", () => {
+  let tree = renderer.create(<Footer pos="absolute" />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+    <footer
+      className="bottom-0 border-gray-200 w-full px-2 sm:px-4 py-2.5 bg-[#131313] absolute"
     >
-      Copyright © 2022 Bearz
-    </p>
-  </div>
-</footer>
-`);
-})
+      <div
+        className=" container flex flex-wrap justify-center items-center container mx-auto max-w-6xl"
+      >
+        <p
+          className="text-white font-bold"
+        >
+          Copyright © 2022 Bearz
+        </p>
+      </div>
+    </footer>
+  `);
+});
