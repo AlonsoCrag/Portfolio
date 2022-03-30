f0f7040b557872bd5010cf66694cbdfda21440cc
Alonso <alonsocadev@gmail.com>
2022-03-30 11:11:57 -0600

    PWA update

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
+
+const main = async () => {
+    console.log("GGG ASYNC")
+}
+
 let elm = ["a"]
 
 let elm_two = ["a"]
 
 console.log(elm == elm_two)
+console.log(elm == elm_two, 'askjdjas')
+main()
+console.log(elm == elm_two, 'askjdjas')
